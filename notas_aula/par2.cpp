e1d8d76ec4222df7ee916302b0837de983065e06
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-05-05 20:52:59 -0300

    Adicionado código alternativo a par

---
+++ notas_aula/par2.cpp
@@ -0,0 +1,14 @@
+//página 19, exemplo 4 
+
+#include <iostream>
+using namespace std;
+	int main () {
+int num;
+cout << "Entre com um número inteiro:";
+	cin >> num;	
+if(num %2 == 0)
+	cout << "O número eh par\n";
+else
+	cout << "O numero eh impar\n";
+
+}
