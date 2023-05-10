aba5d14b9121db43c7b2240065448fb626d30360
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-05-09 23:02:23 -0300

    Primeiro Hello World sem namespace std

---
+++ meus_projetos/Nonamespc.cpp
@@ -0,0 +1,6 @@
+// Hello world mais eficiente que escrevi em C++, aprendi a fazer variáveis sem depender do parâmetro "using namespace std"
+#include <iostream>
+int main() {
+std::cout << "Hello world without namespace std";
+
+}
